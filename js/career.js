913cd9992b10e99085b9188ac14c4b8388649b20
davidnicholastaylor <dntaylor14@gmail.com>
2018-07-18 15:58:25 -0500

    added loop and populated html

--- js/career.js
+++ js/career.js
@@ -1,62 +1,66 @@
 
 //////////////////////  Database Const
 
-const career = {}
+const career = {
 
 //////////////////////  Array - Intro
 
-career.intro = [
+"intro": [
     {
         name: "Dewey Cox",
         description: "He does a lot of good arts, and makes all the people's happy."
     }
-]
+],
 
 //////////////////////  Array - Skills
 
-career.skills = [
+"skills": [
     {
     name: "Fire Juggling",
     description: "Master of the flaming baton."
     }
-]
+],
+
 //////////////////////  Array - Published
 
-career.published = [
+"published": [
     {
     name: "Hotter than the Sun",
     description: "Best-selling novel about recovering from a tragic fire-swallowing trick."
     }
-]
+],
 
 //////////////////////  Array - Residence
 
-career.residence = [
+"residence": [
     {
     name: "Hawaii Beachside Property",
     description: "A small shack on the coast of Maui.",
     beginTime: "1988",
     endTime: "2001"
     }
-]
+],
 
 //////////////////////  Array - Collaborators
 
-career.collaborators = [
+"collaborators": [
     {
     name: "Joan Rivera",
     description: "Scandal artist that boosted his career with badmouthing and smut-stirring."
     }
-]
+],
 
 //////////////////////  Array - Awards
 
-career.awards = [
+"awards": [
     {
     name: "Woody for Best Erotic Album",
     description: "Dewey won this award for his chart-topping album, 'Going down, Getting rough'"
     }
 ]
+}
+
+//////////////////////  Stringify & Parse JSON
 
 const saveCareerData = function (databaseObject, localStorageKey) {
     const stringifiedCareerData = JSON.stringify (databaseObject)
@@ -70,7 +74,21 @@ const loadCareerData = function (localStorageKey) {
 
 saveCareerData(career, "Career")
 
-loadCareerData("Career")
+const careerJSONData = loadCareerData("Career")
+
+//////////////////////  Populate HTML w/JSON
+
+var artistIntro = document.getElementsByClassName("int")[0];
+var artIntro = careerJSONData.intro ; 
+
+for(var i = 0; i < artIntro.length; i++) {
+    var h3 = document.createElement("h3");
+    h3.innerHTML = artIntro[i].name;
+    artistIntro.appendChild(h3);
+    var p = document.createElement("p");
+    p.innerHTML = artIntro[i].description;
+    artistIntro.appendChild(p);
+}
 
 
 console.log(career)
