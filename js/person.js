65f9043c7d91b39a5aa379d5feff6d1afd252903
Figamus <fig74656@yahoo.com>
2018-07-18 15:12:00 -0500

    Added image. Have js object done

--- js/person.js
+++ js/person.js
@@ -1,4 +1,19 @@
-const person {
-    name: "Dewey Cox",
-    birthplace: "Springberry, Alabama"
+const person = {
+    born: "1936",
+    birthPlace: "Springberry, Alabama,",
+    name: "Dewford Randolph Cox",
+    died: "2007",
+    hairColor: "Brown",
+    height: `6'2"`,
+    weight: "180lbs",
+    family: [Edith, ]
 }
+
+// var objects = [
+// {object1},
+// {object2},
+// {object3},
+// {object4},
+// ]
+
+// console.og(objects[0].event)
