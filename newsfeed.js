4fee5d03be6d778a89f839f75fd2a2e004dd3d76
Jessteus <mateusvanhalen@gmail.com>
2018-07-18 15:58:19 -0500

    added text with js

--- newsfeed.js
+++ newsfeed.js
@@ -1,6 +1,6 @@
 //related image, date, location, type of event, collaborators, and where the event occured or to purchase tickets.
 
-const events = [{
+const news = [{
     event: "New album release",
     date: "July 20th at 10",
     location: "Nashville",
@@ -38,7 +38,32 @@ const events = [{
     rating: "10 out of 10. Great form goin in the drink",
     tickets: "No way, brah. Too soggy"
 
-}]
+  }
+]
 
+const saveNewsBase = function (databaseObject, localStorageKey) {
+const stringifiedDatabase = JSON.stringify(databaseObject)
+localStorage.setItem(localStorageKey, stringifiedDatabase)
+}
 
+const loadNewsData = function (localStorageKey) {
+const databaseString = localStorage.getItem(localStorageKey)
+return JSON.parse(databaseString)
+}
 
+saveNewsBase(news, "News")
+
+const newsJSONdata =loadNewsData("News")
+
+console.log(news)
+
+var newsRecap = document.getElementsByClassName("news")[0];
+var items = newsJSONdata;
+
+for(var i = 0; i < items.length; i++) {
+    var h2 = document.createElement("h2");
+    h2.innerHTML = items[i].event;
+    newsRecap.appendChild(h2);
+    var p = document.createElement("p");
+    p.innerHTML = items[i].date;
+    newsRecap.appendChild(p);}
