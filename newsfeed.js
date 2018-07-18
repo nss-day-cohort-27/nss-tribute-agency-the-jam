649e72ff9b7ffbc49edb36687bd9fd51510673a9
Jessteus <mateusvanhalen@gmail.com>
2018-07-18 13:52:30 -0500

    add news arrary

---
+++ newsfeed.js
@@ -0,0 +1,44 @@
+//related image, date, location, type of event, collaborators, and where the event occured or to purchase tickets.
+
+const events = [{
+    event: "New album release",
+    date: "July 20th at 10",
+    location: "Nashville",
+    type: "concert",
+    collaborators: "Brenda Long",
+    rating: "7 out of 10",
+
+},
+{    
+    event: "Dewey walks in on drug usage in batchroom",
+    date: "Back in the day",
+    location: "Memphis",
+    type: "concert",
+    collaborators: "Tim Meadows",
+    rating: "1 out of 10. You Don't want no part of this Dewey",
+    tickets: "Some box office in Memphis"
+
+},
+    {
+    event: "Dewey cuts his big brother in 2",
+    date: "Flashback",
+    location: "Barn",
+    type: "accident",
+    collaborators: "slingblade",
+    rating: "9 out of 10. That was funny. No way he would have still been alive",
+    tickets: "Doubtful"
+  
+},
+{
+    event: "Dewek drives tractor in water",
+    date: "Thursday",
+    location: "Lake",
+    type: "plunge",
+    collaborators: "gravity",
+    rating: "10 out of 10. Great form goin in the drink",
+    tickets: "No way, brah. Too soggy"
+
+}]
+
+
+
