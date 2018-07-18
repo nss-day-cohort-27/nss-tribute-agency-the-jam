8418480de759d35341b8b83963d06b38996985bb
davidnicholastaylor <dntaylor14@gmail.com>
2018-07-18 10:48:18 -0500

    initialize branch

---
+++ career.js
@@ -0,0 +1,22 @@
+//////////////////////  Database Array
+
+const career = {}
+
+career.intro = []
+career.skills = []
+career.published = []
+career.residence = []
+career. collaborators = []
+career.awards = []
+
+//////////////////////  Array Objects - Intro
+
+//////////////////////  Array Objects - Skills
+
+//////////////////////  Array Objects - Published
+
+//////////////////////  Array Objects - Residence
+
+//////////////////////  Array Objects - Collaborators
+
+//////////////////////  Array Objects - Awards
